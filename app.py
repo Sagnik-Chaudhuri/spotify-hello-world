761728a56e88966101cc516b2d995cd87c637ffc
Sagnik-Chaudhuri <csagnik.chaudhuri@gmail.com>
2021-01-26 21:35:40 +0530

    completed login; added callback, refresh routes

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
+from helpers import config
+from requests.api import request
+from urllib.parse import urlencode
+import requests
+import secrets
+import string
+import logging
+import json
 import os
-from flask import Flask
-from flask import request
+from flask import (
+    abort,
+    Flask,
+    make_response,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(config.SESSION_SECRET_KEY_LENGTH)
 
 
 @app.route('/', methods=['OPTIONS'])
@@ -10,7 +28,82 @@ def options():
     return {}, 200
 
 
-@app.route('/hello', methods=['POST'])
-def main():
-    print('\nHello World')
-    return {'response': 'hello'}, 200
+@app.route('/login', methods=['GET'])
+def login():
+    auth_state = ''.join(secrets.choice(
+        string.ascii_letters + string.digits) for i in range(config.RANDOM_STRING_STATE_LENGTH))
+    payload = {
+        'client_id': config.CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': config.REDIRECT_URI,
+        'scope': config.LOGIN_SCOPE,
+        'state': auth_state,
+        'show_dialog': True
+    }
+    response = make_response(
+        redirect(f'{config.SPOTIFY_LOGIN_URL}/?{urlencode(payload)}'))
+    response.set_cookie(config.SPOTIFY_STATE_COOKIE_NAME, auth_state)
+    app.logger.info('\nlogin url response: ', response)
+    return response
+
+    # response = requests.get(
+    #     url=config.SPOTIFY_LOGIN_URL + '/?' + urlencode(req_params))
+    # print('\nspotify login url call resp: ', response.next)
+    # return response.text, 200
+
+
+@app.route('/callback', methods=['GET'])
+def callback():
+    '''OAuth Callback'''
+
+    error = request.args.get('error')
+    code = request.args.get('code')
+    state = request.args.get('state')
+    stored_state = request.cookies.get(config.SPOTIFY_STATE_COOKIE_NAME)
+
+    if state is None or state != stored_state:
+        app.logger.error('Error message: ', repr(error))
+        return {'error': error}, 400
+
+    payload = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': config.REDIRECT_URI,
+    }
+    response = requests.post(url=config.SPOTIFY_TOKEN_URL, data=payload, auth=(
+        config.CLIENT_ID, config.CLIENT_SECRET))
+    response_data = response.json()
+
+    if response.status_code != 200 or response_data.get('error'):
+        app.logger.error(
+            'Failed to receive token: ', response_data.get('error'))
+        return {'error': response_data.get('error')}, response.status_code
+
+    app.logger.info('\ntoken url response: ', response_data)
+
+    session['tokens'] = {
+        'access_token': response_data.get('access_token'),
+        'refresh_token': response_data.get('refresh_token'),
+        'token_expiry': response_data.get('expires_in')
+    }
+
+    return 'Success', 200
+
+
+@app.route('/refresh')
+def refresh():
+    '''Refresh access token.'''
+
+    payload = {
+        'grant_type': 'refresh_token',
+        'refresh_token': session.get('tokens').get('refresh_token'),
+    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    response = requests.post(url=config.SPOTIFY_TOKEN_URL, data=payload, auth=(
+        config.CLIENT_ID, config.CLIENT_SECRET), headers=headers)
+    response_data = response.json()
+    app.logger.info('\nrefresh url response: ', response_data)
+
+    session['tokens']['access_token'] = response_data.get('access_token')
+    return json.dumps(session['tokens'])
---
+++ helpers/config.py
@@ -0,0 +1,11 @@
+CLIENT_ID = 'e51c506eee9140f0888f719e20e10b85'
+CLIENT_SECRET = '7f6afe4206fd4dd7afcb1fabcc78dd42'
+REDIRECT_URI = 'http://localhost:5000/callback'
+
+LOGIN_SCOPE = 'user-read-private user-read-email'
+SPOTIFY_LOGIN_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+RANDOM_STRING_STATE_LENGTH = 16
+SPOTIFY_STATE_COOKIE_NAME = 'spotify_auth_state'
+
+SESSION_SECRET_KEY_LENGTH = 16
